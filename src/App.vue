34d3d4817d10f8254cb849488486db0e105bd25e
Tobias Rocha da Rosa <tobias.rosa@ingaia.com.br>
2022-02-20 23:17:36 -0300

    create header component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <Header :logo="require('./assets/logo.svg')" />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <header class="header">
+    <h1 class="header__logo">
+      <img :src="logo" alt="Ubook logo" />
+    </h1>
+    <form class="header__form">
+      <input
+        type="text"
+        class="header__inputSearch"
+        :placeholder="placeholder"
+      />
+    </form>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      placeholder: "Buscar",
+    };
+  },
+  props: {
+    logo: {
+      required: true,
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 16px;
+
+  &__logo {
+    width: 148px;
+    height: 32px;
+  }
+
+  &__form {
+    display: flex;
+    position: relative;
+    width: calc(100% - 228px);
+
+    &::before {
+      background-image: url("../assets/ic-search.svg");
+      content: "";
+      position: absolute;
+      top: 15px;
+      right: 15px;
+      width: 16px;
+      height: 16px;
+    }
+  }
+
+  &__inputSearch {
+    border: 0;
+    border-radius: 4px;
+    background-color: #e4e7f4;
+    padding: 7px 35px 6px 8px;
+    width: 100%;
+    height: 32px;
+
+    &:focus-visible {
+      outline: none;
+    }
+  }
+}
+</style>
